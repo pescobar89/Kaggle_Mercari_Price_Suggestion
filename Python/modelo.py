1e51b29b3008d6e387a2dc211aaf03b4abc4a450
Pablo Escobar <pabloev89@gmail.com>
2017-12-04 18:09:37 -0500

    Estructuracion inicial, comenzando a explorar variables independientes

--- Python/modelo.py
+++ Python/modelo.py
@@ -1,11 +1,19 @@
+#------------------------------------------------------------------------------
+# 0. SETUP 
+#------------------------------------------------------------------------------
+# Librerias
 import os
 import pandas as pd
+import numpy as np
 import dplython as dp
 import gc
 
 # Fijamos el directorio de trabajo
-os.chdir('D:/pescobar/otros/Kaggle/Mercari_Price_Suggestion/')
+os.chdir('D:/pescobar/otros/Kaggle/Kaggle_Mercari_Price_Suggestion/')
 
+#------------------------------------------------------------------------------
+# 1. LECTURA Y FORMATEO INICIAL
+#------------------------------------------------------------------------------
 # Leemos las bases de datos
 train = pd.read_csv('data/train.tsv', sep='\t')
 test = pd.read_csv('data/test.tsv', sep='\t')
@@ -18,12 +26,26 @@ test.columns = ['id'] + test.columns.tolist()[1:len(test.columns.tolist())]
 train = dp.mutate(train, subset='train')
 test = dp.mutate(test, subset='test')
 # Añadimos la variable precio al test con valores nan
-test = dp.mutate(test, price=float('nan'))
+test = dp.mutate(test, price=np.nan)
 # Reordenamos las variables
 test = test[train.columns.tolist()]
 # Unimos los dos data frames en uno
 df = train.append(test)
+# Reseteamos los indices
+df = df.reset_index(drop=True)
 # Borramos los df originales
 del test, train
 gc.collect()
+# Reorganizamos el orden de las variables
+orden = ['subset', 'id', 'price', 'item_condition_id', 'shipping',
+         'brand_name', 'category_name', 'name', 'item_description']
+df = df[orden]
+del orden
 
+#------------------------------------------------------------------------------
+# 2. TRANSFORMACIONES INICIALES DE VARIABLES
+#------------------------------------------------------------------------------
+# A partir de la variable category_name generamos variables por cada nivel de 
+# categoria
+categorias = df.category_name.str.split('/', expand=True)
+categorias.fillna(value=np.nan, inplace=True)
